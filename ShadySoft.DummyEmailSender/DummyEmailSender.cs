89ef8bad6c3ecd4141f6ab09c3f5dcd3e6de8200
Brady <bradym@outlook.com>
2019-05-21 16:33:27 -0500

    Add project files.

---
+++ ShadySoft.DummyEmailSender/DummyEmailSender.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace ShadySoft.DummyEmailSender
+{
+    public class DummyEmailSender : IEmailSender
+    {
+        public Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            Debug.WriteLine("************************************************");
+            Debug.WriteLine($"Recipient: {email}");
+            Debug.WriteLine("************************************************");
+            Debug.WriteLine($"Subject: {subject}");
+            Debug.WriteLine("************************************************");
+            Debug.WriteLine(htmlMessage);
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ ShadySoft.DummyEmailSender/Extensions/DependencyInjection/ServiceCollectionExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShadySoft.DummyEmailSender.Extensions.DependencyInjection
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddDummyEmailSender(this IServiceCollection services)
+        {
+            return services.AddScoped<IEmailSender, DummyEmailSender>();
+        }
+    }
+}
